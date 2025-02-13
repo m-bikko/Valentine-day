f065547ab4c56442ea9fdb8a91db88ca3eb7cdd4
Beimbet <beimbet.maratov@mail.ru>
2025-02-14 03:11:19 +0500

    Last Update

--- src/App.jsx
+++ src/App.jsx
@@ -48,12 +48,16 @@ export default function App() {
                 </motion.button>
             </div>
             {showPopup && (
-                <motion.div className="popup" initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+                <motion.div className="popup" initial={{opacity: 0}} animate={{opacity: 1}}>
                     <h2 className={"clr"}>Beimbet inviting to see you at:</h2>
-                    <a href="https://2gis.kz/almaty/search/%D0%BA%D1%83%D1%88%D0%B0%D1%82%D1%8C/firm/9429940001525559?m=76.95642%2C43.245285%2F20" target="_blank" rel="noopener noreferrer">
+                    <a href="https://2gis.kz/almaty/search/%D0%BA%D1%83%D1%88%D0%B0%D1%82%D1%8C/firm/9429940001525559?m=76.95642%2C43.245285%2F20"
+                       target="_blank" rel="noopener noreferrer">
                         Luckee Yu, 19th February at 19:00
                     </a>
-                    <img src={PintaImage} alt="Pinta Interior" className="pinta" />
+                    <img src={PintaImage} alt="Pinta Interior" className="pinta"/>
+                    <div>
+                        Make a screenshot and share to me, my Dear
+                    </div>
                 </motion.div>
             )}
         </div>
